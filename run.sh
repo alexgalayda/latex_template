2a26fe8e8354d086e6fea91110db5f27d3d4a797
sanya <alexgalayda@gmail.com>
2020-09-30 23:08:53 +0300

    hot fix

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#cp Makefile ..
-#cp example.env ..
+cp Makefile ..
+cp example.env ..
 cp doc.tex ../main.tex
-#cp -r assets ..
-#cp -r sections ..
-#sed -i 's/config.env/example.env/' ../Makefile
+cp -r assets ..
+cp -r sections ..
+sed -i 's/config.env/example.env/' ../Makefile
 sed -i 's/\\input{sections\/example}/%/' ../main.tex
